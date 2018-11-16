04a442a766e16c81952f2fcacedd8ec40e3794f3
dh_a84dt2 <dh_a84dt2@ps604030.dreamhostps.com>
2018-11-15 23:29:36 -0800

    finalized the view

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports= {
- database: "mongodb://localhost:27017/user_login_test",
+ database: "mongodb://Appsters321:Appsters321@ds235788.mlab.com:35788/reporting-system",
  secret: 'somethingGood'
 };
